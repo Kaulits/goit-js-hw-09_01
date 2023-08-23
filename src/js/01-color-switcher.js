076b072f36205b5f63832e9ed5e3385008b05997
Kaulits <lehgran@gmail.com>
2023-08-23 23:44:37 +0300

    task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    bodyEl: document.body,
+}
+
+let colorSwitch;
+
+
+refs.startBtn.addEventListener('click', startBtnClick);
+refs.stopBtn.addEventListener('click', stopBtnClick);
+
+function startBtnClick() {
+    refs.startBtn.disable = true;
+    colorSwitch = setInterval(() => {
+        refs.bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+};
+
+function stopBtnClick() {
+    refs.startBtn.disable = false;
+    clearInterval(colorSwitch);
+};
